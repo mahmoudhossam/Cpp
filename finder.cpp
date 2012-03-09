6515c02c72c9a57e7512be43b9dc45de8de041e9
Mahmoud Hossam <mahmoudhossam@users.noreply.github.com>
2012-03-09 07:26:26 +0200

    Capitalization in finder

--- finder.cpp
+++ finder.cpp
@@ -7,16 +7,16 @@ int main()
 	std::string src;
 	std::string pattern;
 	std::cout << "Welcome to string finder!\n" << std::endl;
-	std::cout << "please,enter a string: ";
+	std::cout << "Please,enter a string: ";
 	std::cin >> src;
-	std::cout << "string length: " << src.size() << std::endl;
-	std::cout << "now enter a keyword to search for: ";
+	std::cout << "String length: " << src.size() << std::endl;
+	std::cout << "Now enter a keyword to search for: ";
 	std::cin >> pattern;
 	if(src.find(pattern) == std::string::npos){
 		std::cout << "Sorry,the keyword you're searching for cannot be found.\n";
 
 	} else {
-		std::cout << "a match was found at position " << src.find(pattern)+1 << std::endl;
+		std::cout << "A match was found at position " << src.find(pattern)+1 << std::endl;
 	}
 
 }
